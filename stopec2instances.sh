fb37f067b0c023571bf2e4040b3e17a1bc3e328a
Epronobis8 <Epronobis8@gmail.com>
2019-11-27 09:12:41 -0500

    Working files to github

---
+++ stopec2instances.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env python
+# have to add the stopinator tag and the value = stop or terminate.
+
+import boto3
+
+# Connect to the Amazon EC2 service
+ec2 = boto3.resource('ec2')
+
+# Loop through each instance
+for instance in ec2.instances.all():
+  state = instance.state['Name']
+  for tag in instance.tags:
+
+    # Check for the 'stopinator' tag
+    if tag['Key'] == 'stopinator':
+      action = tag['Value'].lower()
+      
+      # Stop?
+      if action == 'stop' and state == 'running':
+        print "Stopping instance", instance.id
+        instance.stop()
+      
+      # Terminate?
+      elif action == 'terminate' and state != 'terminated':
+        print "Terminating instance", instance.id
